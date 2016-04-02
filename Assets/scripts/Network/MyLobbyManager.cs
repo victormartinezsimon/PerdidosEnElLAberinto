0ce913d813a193f85b1e08414606317985940008
victor martinez simon <victormartinezsimon@gmail.com>
2016-04-02 17:31:39 +0200

    start the lobby

---
+++ Assets/scripts/Network/MyLobbyManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyLobbyManager : NetworkLobbyPlayer
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/Network/MyNetworkLobbyPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkLobbyPlayer : NetworkLobbyPlayer
+{
+
+
+    [SyncVar(hook = "OnMyName")]
+    public string playerName = "";
+
+    public void OnMyName(string newName)
+    {
+        playerName = newName;
+    }
+
+    public void OnReadyClicked()
+    {
+        SendReadyToBeginMessage();
+    }
+
+}
